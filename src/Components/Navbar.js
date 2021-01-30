d01fb56265b26beff9f21dad05f9d3e3f0620897
TheMattyDGWU <themattydgwu@gmail.com>
2021-01-29 22:21:49 -0500

    changed initials to callsign and reordered navBar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Style from './Navbar.module.scss';
 import Toggler from "./Toggler";
 import classNames from 'classnames';
-import {info} from '../Info/Info';
+import { info } from '../Info/Info';
 
-export default function Navbar({darkMode, handleClick}) {
+export default function Navbar({ darkMode, handleClick }) {
 
 
     return (
         <nav>
             <ul className={classNames(Style.navList, darkMode ? Style.dark : null)}>
+                <li><NavLink exact activeClassName="none" to='/'><span className={Style.logo}>{info.initials}</span></NavLink></li>
                 <li><NavLink exact activeClassName="current" to='/'>Home</NavLink></li>
                 <li><NavLink exact activeClassName="current" to='/about'>About Me</NavLink></li>
-                <li><NavLink exact activeClassName="none" to='/'><span className={Style.logo}>{info.initials}</span></NavLink></li>
                 <li><NavLink exact activeClassName="current" to='/portfolio'>Portfolio</NavLink></li>
-                <Toggler darkMode={darkMode} handleClick={handleClick}/>
+                <Toggler darkMode={darkMode} handleClick={handleClick} />
             </ul>
         </nav>
     )
